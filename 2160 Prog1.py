4a7495133bb8ba096033bb58448f69eccaafac26
Richard Kumahia <Rkumahia@Gmail.com>
2020-12-19 18:48:50 -0500

    Program 1 of EECE.2160

---
+++ 2160 Prog1.py
@@ -0,0 +1,6 @@
+# First ECE.2160 Program
+# Written in Python
+print('ECE Application Programming')
+print('Spring 2020')
+print('Lecture days: MWF')
+print('Instructors: Dr. Lin Li and Dr. Michael Geiger')
